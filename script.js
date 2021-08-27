7cc5de5c6ea21958c7d67f1d466ea110af442230
JaredAcker <87212179+JaredAcker@users.noreply.github.com>
2021-08-26 23:42:38 -0400

    Update script.js
    creating another variable with events for each time of the day

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ $(function () {});
 var today = moment().format("dddd, MMMM Do");
 var now = moment().format("H A");
 
+/*creating another variable with events for each time of the day */
+var planWorkday = [
+    { time: "9 AM", event: "" },
+    { time: "10 AM", event: "" },
+    { time: "11 AM", event: "" },
+    { time: "12 PM", event: "" },
+    { time: "1 PM", event: "" },
+    { time: "2 PM", event: "" },
+    { time: "3 PM", event: "" },
+    { time: "4 PM", event: "" },
+    { time: "5 PM", event: "" },
+  ];
